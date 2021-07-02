deb49b919ab42ea83e1488b8b9043a6d15260923
es5es5 <es5e5s@nate.com>
2021-07-02 15:03:25 +0900

    searchBusLaneAJAX

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <input type="text" v-model="busNo">
+    <button type="button" @click="searchBusLaneAJAX">searchBusLaneAJAX</button>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import axios from 'axios'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'App',
+  data () {
+    return {
+      busNo: '10',
+      apiKey: encodeURI('uiRepkko8TxYUyVP2KqYeg')
+    }
+  },
+  methods: {
+    searchBusLaneAJAX () {
+      axios.get(
+        `https://api.odsay.com/v1/api/searchBusLane?apiKey=${this.apiKey}&busNo=${this.busNo}`
+      ).then(result => {
+        console.log(result)
+      })
     }
   }
 }
+</script>
+
+<style lang="scss">
 </style>
---
+++ vue.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack')
+
+// vue.config.js
+module.exports = {
+  publicPath: process.env.VUE_APP_BASE_URL || '/',
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        // moment: 'moment',
+      })
+    ]
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        // prependData: `
+        //   @import "~@/assets/scss/variable.scss";
+        // `
+      }
+    }
+  },
+  devServer: {
+    host: '0.0.0.0',
+    https: false,
+    disableHostCheck: true,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+}
